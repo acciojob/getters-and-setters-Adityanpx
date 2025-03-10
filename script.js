a19515a512daeec13dcfbfcb1185ea5bf3da031a
Adityanpx <137485127+Adityanpx@users.noreply.github.com>
2025-03-10 10:06:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,41 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name , age){
+		this.name = name;
+		this.age = age;
+	}
 
-class Student extends Person {}
+	getName(){
+		return this.name;
+	}
 
-class Teacher extends Person {}
+	getAge() {
+		return this.age;
+		
+	}
+
+	setName(newAge){
+		if (newAge > 0) {
+			this.name = newAge;
+			
+		}else {
+			console.log("Age must be a positive number");
+		}
+	}
+	
+}
+
+class Student extends Person {
+	study(){
+		console.log(`${this.name} is studying`)
+	}
+}
+
+class Teacher extends Person {
+	teach() {
+		console.log(`${this.name} is teaching`)
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
